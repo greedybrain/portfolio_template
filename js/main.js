2e87aba5167dce8e3e0b0919f35437346a12e3c5
Naya Willis <greedybrain18@gmail.com>
2019-11-12 16:31:25 -0500

    menu button, javascript, rotate

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// select dom items
+const menu_btn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menu_nav = document.querySelector(".menu-nav");
+const menu_branding = document.querySelector(".menu-branding");
+const nav_items = document.querySelectorAll(".nav-item");
+
+// set initial state of menu
+let show_menu = false;
+
+menu_btn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	if (!show_menu) {
+		menu_btn.classList.add("close");
+		menu.classList.add("show");
+		menu_nav.classList.add("show");
+		menu_branding.classList.add("show");
+		nav_items.forEach(item => item.classList.add("show"));
+
+		// reset menu state
+		show_menu = true;
+	} else {
+		menu_btn.classList.remove("close");
+		menu.classList.remove("show");
+		menu_nav.classList.remove("show");
+		menu_branding.classList.remove("show");
+		nav_items.forEach(item => item.classList.remove("show"));
+
+		// set menu state
+		show_menu = false;
+	}
+}
